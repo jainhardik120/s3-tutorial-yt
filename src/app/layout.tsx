22458a60e272f966172a43a39b05e8f4c335c04a
Hardik Jain <jainhardik120@gmail.com>
2025-05-11 15:54:13 +0530

    Empty NextJS Project with ShadcnUI

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="p-4">
+        {children}
+      </body>
+    </html>
+  );
+}
